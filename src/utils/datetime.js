427794770b8a1cec64fe03ee170f6b7fdaf4db91
Vu Truong Anh <truonganhvu205@gmail.com>
2025-10-02 11:10:39 +0700

    add script

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,6 +1,6 @@
 const datetime = new Date()
 
-const utc = now.getTime() + now.getTimezoneOffset() * 60000
+const utc = datetime.getTime() + datetime.getTimezoneOffset() * 60000
 const ict = new Date(utc + 7 * 3600000)
 
 let hours = ict.getHours()
